d8de18923c43fa04a679d000052b65484ea12580
François Madene <fmadene@mail.ks.int>
2018-07-17 17:31:51 +0200

    app skeleton

---
+++ src/client/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// Import style
+import './app.css';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="cookingBook">
+		Yo!
+      </div>
+    );
+  }
+}
---
+++ src/server/index.js
@@ -0,0 +1,69 @@
+// Server entry point
+// =========================
+const chalk = require('chalk'); // to have pretty colorful logs in the console
+const express = require('express');
+const mongoose = require('mongoose'); // to easily manage mongoDB
+const bodyParser = require('body-parser'); // Analyzes incoming request bodies in a middleware before our managers (req.body)
+const methodOverride = require('method-override'); // allows use of HTTP verbs such as PUT or DELETE in places where the client does not support it
+const createLogger = require('morgan'); // to log
+const cookieSession = require('cookie-session'); // cookies management
+// const csrfProtect = require('csurf'); // Create a middleware for CSRF token creation and validation (Node.js CSRF protection middleware)
+
+// Connection to mongoDB
+const options = {
+  server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
+  replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }
+};
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/menus', options);
+
+// indicates if the connection failed because it's painful to diagnose
+const database = mongoose.connection;
+database.on('error', () => {
+  console.error(chalk`{red ✘ CANNOT CONNECT TO mongoDB DATABASE menus!}`);
+});
+
+// We export a registration function of a successful login callback, 
+// if it interests the caller (`index.js` uses it to log that the connection is ready)
+const listenToConnectionOpen = function (onceReady) {
+  if (typeof onceReady === 'function') {
+    database.on('open', onceReady);
+  }
+};
+
+// Connection to DB
+listenToConnectionOpen(() => {
+  console.log(chalk`{green ✔ Connection established to mongoDB database}`);
+
+  database.db.listCollections().toArray((err) => {
+    if (err) console.log(err);
+  });
+
+  // Configuration & middleware for all environments (dev, prod,...)
+  const PORT = Number(process.env.PORT) || 8080;
+
+  // Creates the main web app container (`app`), connects the HTTP server to it (` index`)
+  // and determines the full path of the static assets
+  const app = express();
+  app.use(express.static('dist'));
+  const isDev = app.get('env') === 'development';
+  app.use(createLogger(isDev ? 'dev' : 'combined'));
+
+  app.use(bodyParser.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+  app.use(bodyParser.json()); // parse application/json
+  app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+  // app.use(methodOverride(req => req.body._method));
+  app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+  // `cookieSession` stores the entire cookie session
+  app.use(cookieSession({
+    name: 'cooking_book:session',
+    secret: 'Funnybobby is in the place!',
+  }));
+  // app.use(csrfProtect());
+
+  // routes ======================================================================
+  require('../routes')(app);
+
+  // Effective server launch by listening on the correct port for incoming HTTP connections.
+  app.listen(PORT, () => { console.log(chalk`{green ✔ Server listening on port} {cyan ${PORT}}`); });
+});
